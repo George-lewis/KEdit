6d9fe402fc5647169e27ad5fd1c869f1f144541a
George-Lewis <georgejeffreylewis@gmail.com>
2019-04-10 00:27:01 -0400

    Create files and boilerplate code in Main & Controller

---
+++ src/Main.kt
@@ -0,0 +1,46 @@
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import java.net.URL
+
+// Global App reference
+lateinit var app: App
+
+// Application entry point
+fun main(args: Array<String>) {
+
+    Application.launch(App::class.java, *args)
+
+}
+
+class App: Application() {
+
+    init {
+
+        app = this
+
+    }
+
+    lateinit var stage: Stage
+    lateinit var scene: Scene
+    lateinit var root: Pane
+
+    override fun start(stage: Stage?) {
+
+        this.stage = stage!!
+
+        root = FXMLLoader.load(URL("MainLayout.fxml"))
+
+        scene = Scene(root)
+
+        scene.stylesheets.add("application.css")
+
+        stage.scene = scene
+
+        stage.showAndWait()
+
+    }
+
+}
---
+++ src/MainController.kt
@@ -0,0 +1,15 @@
+import javafx.scene.Scene
+import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import kotlin.test.todo
+
+class MainController {
+
+    // Run when the .fxml is loaded
+    fun initialize() {
+
+        TODO("Implement controller init")
+
+    }
+
+}
